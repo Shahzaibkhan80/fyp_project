629ae19100a7cbb1e67a30db67e3b893d430aa16
Shahzaibkhan80 <kamshahzaib@gmail.com>
2025-02-08 14:43:12 +0500

    Updated Strings

---
+++ lib/constant/appStrings/appString.dart
@@ -0,0 +1,41 @@
+class Appstrings {
+  ///App Bar
+  static const String appBarForgotTitle = "Forgot Password";
+  static const String appBarVerification = "Verification Required";
+  static const String appBarConfirmPassword = "ConfirmPassword";
+
+  /// Auth screen
+
+  static const String authDoctorName = "Doctor";
+  static const String authpatientName = "Patient";
+  static const String authloginText = "Log In Account";
+  static const String authEmail = "Email";
+  static const String authPassword = "Password";
+  static const String authForgotPassword = "Forgot Password?";
+  static const String authloginButton = "Log In";
+  static const String authloginReg = "Register";
+  static const String authDontHaveAccount = "Don't have an account?";
+  static const String authForgotScreenText =
+      "Enter your email address to receive verification code";
+  static const String authForgorSendButton = "Send";
+  static const String authVerificationScreenText =
+      "Enter the OTP code we have send you on your provided phone number";
+  static const String authVerificationDontReceiveOtpText =
+      "Didn't receive the OTP?";
+  static const String authVerificationResendText = "Resend";
+  static const String authVerificationButton = "Verify";
+  static const String authCreateNewPasswordText = "Create New Password";
+  static const String authConfirmPasswordScreenText =
+      "Your new password must be different from previous used password";
+  static const String authConfirmPasswordNewPassword = "Confirm Password";
+  static const String authConfirmPasswordSaveButton = "Save";
+
+  static const String authRegisterAsPatientText = "Register as Patient";
+  static const String authRegisterName = "Name";
+  static const String authRegisterContactNo = "Contact No";
+  static const String authRegisterGender = "Choose An Option";
+  static const String authRegisterAge = "Age";
+  static const String authRegisterButton = "Register";
+  static const String authRegisterAlreadyHaveText = "Already have an account?";
+  static const String authRegisterLoginText = "Log In";
+}
